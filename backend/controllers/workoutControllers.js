422fc72563650baa6b58757a04b4b4e3cb356555
purniawan4905 <purniawan4905@gmail.com>
2024-08-09 09:52:58 +0700

    initial Commit

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 // get all workouts
 const getWorkouts = async (req, res) => {
     const user_id = req.user._id
-    const workouts = await Workout.find({user_id}).sort({createAt: -1})
+
+    const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 })
 
     res.status(200).json(workouts)
 }
@@ -13,44 +14,46 @@ const getWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
     const { id } = req.params
 
-    const workout = await Workout.findById(id)
-
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "No such workout"})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
-    if(!workout){
-        return res.status(404).json({error: 'No such workout'})
+    const workout = await Workout.findById(id)
+
+    if (!workout) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
     res.status(200).json(workout)
 }
 
-// create a new workout
-const createWorkout = async(req, res) => {
-    const {title, load, reps} = req.body
 
-    let emptyFields=[]
-    if(!title){
+// create new workout
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body
+
+    let emptyFields = []
+
+    if (!title) {
         emptyFields.push('title')
     }
-    if(!load){
+    if (!load) {
         emptyFields.push('load')
     }
-    if(!reps){
+    if (!reps) {
         emptyFields.push('reps')
     }
-    if(emptyFields.length > 0){
-        return res.status(400).json({error: 'Please fill in all fields', emptyFields})
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
     }
 
     // add doc to db
     try {
         const user_id = req.user._id
-        const workout = await Workout.create({title, load, reps, user_id})
+        const workout = await Workout.create({ title, load, reps, user_id })
         res.status(200).json(workout)
-    } catch(error){
-        res.status(400).json({error: error.message})
+    } catch (error) {
+        res.status(400).json({ error: error.message })
     }
 }
 
@@ -58,34 +61,38 @@ const createWorkout = async(req, res) => {
 const deleteWorkout = async (req, res) => {
     const { id } = req.params
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "No such workout"})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
-    const workout = await Workout.findOneAndDelete({_id: id})
+    const workout = await Workout.findOneAndDelete({ _id: id })
 
-    if(!workout){
-        return res.status(400).json({error: 'No such workout'})
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' })
     }
+
     res.status(200).json(workout)
 }
 
-// update workout
+// update a workout
 const updateWorkout = async (req, res) => {
     const { id } = req.params
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "No such workout"})
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
-    const workout = await Workout.findOneAndUpdate({_id: id},{
+    const workout = await Workout.findOneAndUpdate({ _id: id }, {
         ...req.body
     })
 
-    if(!workout){
-        return res.status(400).json({error: "No such workout"})
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' })
     }
+
     res.status(200).json(workout)
 }
+
 
 module.exports = {
     getWorkouts,
--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const workoutSchema = new Schema({
-    title : {
+    title: {
         type: String,
         required: true
     },
@@ -14,7 +14,11 @@ const workoutSchema = new Schema({
     load: {
         type: Number,
         required: true
+    },
+    user_id: {
+        type: String,
+        required: true
     }
-},{timestamps: true})
+}, { timestamps: true })
 
 module.exports = mongoose.model('Workout', workoutSchema)
